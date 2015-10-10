0658c23e16c2440bab0b1284e32855cd48f151e3
Jeff Trawick <trawick@gmail.com>
2015-10-10 18:05:11 -0400

    initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='venvrun',
+    version='0.1',
+    description='run virtualenv-installed program from outside',
+    scripts=('venvrun',),
+)
